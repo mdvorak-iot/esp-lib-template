52d38d3c468fa090e793e44b2dcf1007e013e5a3
Michal Dvorak <mikee2185@gmail.com>
2022-01-22 18:06:36 +0100

    chore: added infinite loop to the example

--- example/main/lib_template_example_main.c
+++ example/main/lib_template_example_main.c
@@ -1,12 +1,19 @@
 #include "lib.h"
 #include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 static const char TAG[] = "example";
 
-void app_main()
+_Noreturn void app_main()
 {
     lib_example();
 
     // Setup complete
     ESP_LOGI(TAG, "started");
+
+    while (true)
+    {
+        vTaskDelay(1);
+    }
 }
